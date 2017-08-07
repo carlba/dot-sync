855e0ad7e7800752c052c154ea4bd1761b79726c
Carl Bäckström <genzorg@gmail.com>
2017-08-07 23:38:24 +0200

    Corrected GitHub repo path

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -20,7 +20,7 @@ except ImportError:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'carlba/dot_sync'
+GITHUB_REPO = 'carlba/dot-sync'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
